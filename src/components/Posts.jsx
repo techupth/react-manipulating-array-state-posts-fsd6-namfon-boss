7ad6e2574c7e760b7ec6b576f1688277f7e874fa
Namfon <varisa.vb@hotmail.com>
2024-06-06 14:52:58 +0700

    feat(Post,postData): Add useState for counting likes

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,28 +1,50 @@
+import { useState } from "react";
+import postData from "./postData";
+
 function Posts() {
+  const initialLikes = postData.map((data) => data.likes);
+  const [likesNum, setLikesNum] = useState(initialLikes);
+  const handleLikes = (index) => {
+    let newLikesNum = [...likesNum];
+    newLikesNum[index] += 1;
+    setLikesNum(newLikesNum);
+  };
+  const handleDislike = (index) => {
+    let newLikesNum = [...likesNum];
+    newLikesNum[index] = Math.max(0, newLikesNum[index] - 1);
+    setLikesNum(newLikesNum);
+  };
+  // console.log(likesNum);
   return (
-    <div class="app-wrapper">
-      <h1 class="app-title">Posts</h1>
-      <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+    <div className="app-wrapper">
+      <h1 className="app-title">Posts</h1>
+      <div className="post-list">
+        {postData.map((eachPost, index) => (
+          <div className="post-item" key={eachPost.id}>
+            <div className="post-header">
+              <h2>{eachPost.title}</h2>
+              <div className="post-social-media-stats">
+                <span className="stats-topic">Likes: </span>
+                <span className="post-likes">{likesNum[index]}</span>
+              </div>
+            </div>
+            <p className="post-content">{eachPost.content}</p>
+            <div className="post-actions">
+              <button
+                className="like-button"
+                onClick={() => handleLikes(index)}
+              >
+                Like
+              </button>
+              <button
+                className="dislike-button"
+                onClick={() => handleDislike(index)}
+              >
+                Dislike
+              </button>
             </div>
           </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
